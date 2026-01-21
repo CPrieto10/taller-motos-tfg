497c2ce8e458c0238e524521a10847679dbb0284
Carlos Prieto <carlos_prieto10@hotmail.com>
2026-01-21 20:11:09 +0100

    Add entidades Usuario y Moto sin estar vacio

--- backend/src/main/java/com/tfg/backend/entity/Moto.java
+++ backend/src/main/java/com/tfg/backend/entity/Moto.java
@@ -1,4 +1,37 @@
 package com.tfg.backend.entity;
 
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "motos")
 public class Moto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "marca", nullable = false, length = 150)
+    private String marca;
+
+    @Column(name = "modelo", nullable = false, length = 150)
+    private String modelo;
+
+    @Column(name = "cilindrada", nullable = false)
+    private Integer cilindrada;
+
+    @Column (name = "anio_fabricacion", nullable = false)
+    private Integer anio;
+
+    @Column (name = "km_actuales", nullable = false)
+    private Integer kmActuales;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "usuario_id", nullable = false)  // Esta columna se creará en la tabla 'motos'
+    private Usuario usuario;
 }
--- backend/src/main/java/com/tfg/backend/entity/Usuario.java
+++ backend/src/main/java/com/tfg/backend/entity/Usuario.java
@@ -1,4 +1,36 @@
 package com.tfg.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table (name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "email", unique = true,nullable = false, length = 255)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "fecha_alta", nullable = false)
+    private LocalDate fechaAlta;
+
+    /* Si quiero hacerlo bidireccional debo añadir esto
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore  // ← EVITA que se serialice la lista de rutas
+    private List<Moto> motos = new ArrayList<>();
+    */
 }

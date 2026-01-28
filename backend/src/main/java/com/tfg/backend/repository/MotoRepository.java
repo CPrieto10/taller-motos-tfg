8b390adb6250646bf92fdab87e952f3c8d17c353
Carlos Prieto <carlos_prieto10@hotmail.com>
2026-01-28 11:08:01 +0100

    Add repository y añadir consultas personalizadas en repositorios (usuario y moto)

---
+++ backend/src/main/java/com/tfg/backend/repository/MotoRepository.java
@@ -0,0 +1,15 @@
+package com.tfg.backend.repository;
+
+import com.tfg.backend.entity.Moto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MotoRepository extends JpaRepository<Moto,Long> {
+
+    List<Moto> findByUsuarioId(Long usuarioId);
+
+
+}
---
+++ backend/src/main/java/com/tfg/backend/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.tfg.backend.repository;
+
+import com.tfg.backend.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+}
